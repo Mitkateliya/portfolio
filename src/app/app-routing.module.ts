2b82e6d587a13c8686c2c1b05c07618b5bcb86af
Mitkateliya <miteshkateliya10@gmail.com>
2023-06-29 23:53:40 +0530

    added homepage,header,navbar and animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import VanillaTilt from 'vanilla-tilt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'imitkateliya';
+
+  ngOnInit(): void {
+    VanillaTilt.init(document.querySelector(".mainContent") as any, {
+      max:10,perspective:5000,
+    })
+  }
 }
